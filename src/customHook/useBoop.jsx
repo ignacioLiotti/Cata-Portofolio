cea9582c3c4f267fc49dbac5218de5dbb0b6da31
IgnacioLiotti <ignacioliotti@gmail.com>
2022-05-24 16:43:28 -0300

    changes

--- src/customHook/useBoop.jsx
+++ src/customHook/useBoop.jsx
@@ -1,4 +1,4 @@
-import React, {useState,useEffect}from 'react';
+import React, {useState,useEffect, useCallback}from 'react';
 import { useSpring } from 'react-spring';
 function useBoop({
   x = 0,
